cdf547a49fa1a110e00a6fd81aaa24a9df09bcba
artursantana <artursantana843@gmail.com>
2024-01-03 14:48:01 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from './App.styles'
+
+function App() {
+
+  return (
+   <C.Container>
+    <C.Header>
+      <C.HeaderText>
+        Finance System
+      </C.HeaderText>
+    </C.Header>
+    <C.Body>
+      ...
+    </C.Body>
+   </C.Container>
+  )
+}
+
+export default App
